8792a4bdc4f6045abc51f311857c645effe89819
Caleb Rogers <caleb@calebjay.com>
2020-11-08 16:15:50 -0800

    Get ASIN list

--- highlights-getter.js
+++ highlights-getter.js
@@ -1,9 +1,20 @@
 const highlights = [];
+const ASINs = new Set();
 
-document.querySelectorAll('#highlight').forEach((el) => {
-  const metadata = el.closest('.a-spacing-base').querySelector('#annotationNoteHeader').innerText;
-  const highlightText = el.innerText;
-  const title = document.querySelector('h3.kp-notebook-metadata').textContent;
-  const author = document.querySelector('p.kp-notebook-metadata.a-spacing-none').textContent;
-  highlights.push({ metadata, highlightText, title, author });
-});
+
+
+function scrapePage() {
+  document.querySelectorAll('#highlight').forEach((el) => {
+    const metadata = el.closest('.a-spacing-base').querySelector('#annotationNoteHeader').innerText;
+    const highlightText = el.innerText;
+    const title = document.querySelector('h3.kp-notebook-metadata').textContent;
+    const author = document.querySelector('p.kp-notebook-metadata.a-spacing-none').textContent;
+    highlights.push({ metadata, highlightText, title, author });
+  });
+};
+
+function getAllASIN() {
+  document.querySelectorAll('[data-get-annotations-for-asin]').forEach((el) => {
+    ASINs.add(JSON.parse(el.dataset.getAnnotationsForAsin).asin);
+  });
+};
